0b86c2d51129c49668ff37b019c83da625411b6b
Shawn Allen Converse <shawn.a.converse@outlook.com>
2021-03-08 11:52:13 -0800

    w1d1 morning commit

---
+++ lectures_and_demos/week1/d1/comparison.js
@@ -0,0 +1,33 @@
+// Variable
+var name = "Shawn"
+var number = 42
+
+
+var fullName = "Shawn Converse"
+
+
+
+
+console.log(fullName)
+
+
+// Array
+// 0 indexed
+var arr = [1, "spam", True, [1, 2, 3]]
+arr[1]
+// Object
+// Key / Value pairs - Name / Value pairs
+var obj = {
+    name: "Shawn",
+    age: 27
+}
+
+// Conditionals
+if (5 > 2) {
+    console.log("hello")
+} else if (7 < 10) {
+    console.log("seven")
+} else {
+    console.log(7)
+}
+
---
+++ lectures_and_demos/week1/d1/intro_to_python.py
@@ -0,0 +1,89 @@
+# Using Python
+# MAC - python3
+# Windows - python
+
+
+# Python Fundamentals
+
+# Variables
+name = "Shawn"
+number = 42
+# variable name & naming conventions
+spam_spam_spam_spam_spam_spam_spam_spam_spam_spam_spam_spam_ = "eggs"
+dfnadklsfnvadsnfguansngnadsgkluasngnk = "more eggs"
+dfnadklsfnvadsnfguansngnadsgkluasngnk = 42
+dfnadklsfnvadsnfguansngnadsgkluasngnk = True
+
+full_name = "Shawn Converse"
+num = 234817
+grilled_cheese_sandwiches = 451
+
+
+# Primitive Data Types
+# Strings
+# "Shawn"
+# 'spam'
+# 'The quote of the day is: "I like spam"'
+welcome = "Welcome to Python"
+# # Integer
+# 13
+# 42
+cans_of_spam = 275
+# # Booleans
+# True
+# False
+do_i_have_spam = True
+# # Float - decimal
+# 2.5
+# 4.54843514
+pi = 3.14159
+
+print(welcome) # => Welcome to Python
+welcome = "This is another message"
+print(welcome)
+
+
+# Composite Data Type / Collection / Data Structure
+# Lists
+#  0 indexed
+nums = [0, 1, 2, 3, 4, 5]
+
+# Dictionaries
+# Key / Value Pairs
+person = {
+    "name": "Shawn",
+    "age": 27,
+    "interests": ["music", "coding", "video games"],
+    "address": {
+        "street": "123 Shawn St",
+        "state": "CA",
+        "city": "San Jose"
+    }
+}
+
+# Conditionals
+if 7 > 10:
+    print("hello")
+elif 5 > 2:
+    print("seven")
+else:
+    print(7)
+
+# null => None
+# ! => not
+# || => or
+# && => and
+# === => ==
+
+x = True
+y = False
+
+if x and y:
+    print("and statement worked")
+else:
+    print("and did not work")
+
+
+
+
+
