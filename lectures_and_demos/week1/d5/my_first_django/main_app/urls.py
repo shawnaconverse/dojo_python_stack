f63fabd59ddcb618f46f4cc30f32eda962b8000d
Shawn Allen Converse <shawn.a.converse@outlook.com>
2021-03-12 15:49:58 -0800

    w1d5 afternoon commit

--- lectures_and_demos/week1/d5/my_first_django/main_app/urls.py
+++ lectures_and_demos/week1/d5/my_first_django/main_app/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
     # Django auto appends the '/'
     path('', views.index),
     path('success', views.success),
-    path('<str:word>', views.path_word),
+    # path('<str:word>', views.path_word),
+    path("path", views.path),
+    path("redirected_path", views.redirect_path),
     # path('<str:word>/<str:color>', views.display_color),
+    path("dogs", views.dogs)
 ]
 
 # @app.route("/")
--- lectures_and_demos/week1/d5/my_first_django/main_app/views.py
+++ lectures_and_demos/week1/d5/my_first_django/main_app/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Hello There")
 
 
 def success(request):
     return HttpResponse("success!")
 
 
-def path_word(request, word):
-    return HttpResponse(f"You're word is: {word}")
+# def path_word(request, word):
+#     return HttpResponse(f"You're word is: {word}")
+
+
+def path(request):
+    return redirect("/redirected_path")
+
+
+def redirect_path(request):
+    return HttpResponse("This function ran after a redirect!")
+
+
+def dogs(request):
+    context = {
+        "dogs": ["vicky", "shiro", "kenobi"],
+        "name": "Shawn Converse"
+    }
+
+    return render(request, "dogs.html", context)
+
 
