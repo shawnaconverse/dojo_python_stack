bc4dc1efff7be9e557a61743040b33f1e121c8cd
Shawn Allen Converse <shawn.a.converse@outlook.com>
2021-04-14 16:00:16 -0700

    w2d3 afternoon commit

--- lectures_and_demos/w2/d3/orm_project/main_app/models.py
+++ lectures_and_demos/w2/d3/orm_project/main_app/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Dog(models.Model):
     # id = models.IntergerField
     name = models.CharField(max_length = 45)
-    num_of_legs = models.IntegerField()
+    num_of_legs = models.IntegerField(default = 4)
     breed = models.CharField(max_length = 45)
     size = models.CharField(max_length = 45)
+    hair_color = models.CharField(max_length = 45)
+    favorite_toy = models.CharField(max_length = 65, default = "ball")
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
--- lectures_and_demos/w2/d3/orm_project/main_app/views.py
+++ lectures_and_demos/w2/d3/orm_project/main_app/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "dogs": Dog.objects.all() # gives me a list
+    }
 
+    return render(request, 'index.html', context)
+
+
+def create_dog(request):
+    print(request.POST)
+
+    Dog.objects.create(
+        name = request.POST['jasjdjlkfasfhas'],
+        breed = request.POST['breed'],
+        size = request.POST['size'],
+        hair_color = request.POST['hair_color'],
+        favorite_toy = request.POST['toy']
+    )
+
+    return redirect("/")
 
 
