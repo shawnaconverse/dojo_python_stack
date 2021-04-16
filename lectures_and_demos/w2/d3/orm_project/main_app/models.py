7631e720fd03e7ac5c620bb0e856a3299df2ae24
Tyler <thibault.tyler@gmail.com>
2021-04-15 17:03:03 -0700

    one-to-many

--- lectures_and_demos/w2/d3/orm_project/main_app/models.py
+++ lectures_and_demos/w2/d3/orm_project/main_app/models.py
@@ -8,9 +8,19 @@ class Dog(models.Model):
     breed = models.CharField(max_length = 45)
     size = models.CharField(max_length = 45)
     hair_color = models.CharField(max_length = 45)
-    favorite_toy = models.CharField(max_length = 65, default = "ball")
+    # favorite_toy = models.CharField(max_length = 65, default = "ball")
+    # toys = relationship to the class Toy
+
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
-
-
+class Toy(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    size = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    squeakability = models.BooleanField()
+    durability = models.IntegerField()
+    dog = models.ForeignKey(Dog, related_name="toys", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
