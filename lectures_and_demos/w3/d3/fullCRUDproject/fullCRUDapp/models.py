5a9f56e77a41a0cf5f69df89e29e784dd45449ae
Tyler Thibault <thibault.tyler@gmail.com>
2021-04-21 12:37:14 -0700

    upload w3d3 full CRUD app

---
+++ lectures_and_demos/w3/d3/fullCRUDproject/fullCRUDapp/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+
+import bcrypt
+from datetime import datetime
+import re
+
+# Create your models here.
+class UserManager(models.Manager):
+    def registration_validator(self, post_data):
+        errors = {}
+
+        # validate name data
+        if len(post_data['first_name']) < 2:
+            errors['first_name'] = "First name must be at least 2 characters."
+        if len(post_data['last_name']) < 2:
+            errors['last_name'] = "Last name must be at least 2 characters."
+        
+        # validate email
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(post_data['email']):    # test whether a field matches the pattern            
+            errors['email'] = "Invalid email address!"
+        else:
+            user_list = User.objects.filter(email = post_data['email'])
+            if len(user_list) > 0:
+                errors['email'] = "Email is already in use."
+
+        # validate birth date
+        if len(post_data['birth_date']) < 1:
+            errors['birth_date'] = "You must enter a birth date."
+        else:
+            form_date = datetime.strptime(post_data['birth_date'], "%Y-%m-%d")
+            if datetime.now() < form_date:
+                errors["birth_date"] = "You cannot enter a future date for birth date."
+            elif datetime.now().year - form_date.year < 13:
+                errors["birth_date"] = "You must be older than 13 to register"
+
+        # validate password
+        if len(post_data['password']) < 4:
+            errors['password'] = "Password must be at least 4 characters."
+        if post_data['password'] != post_data['confirm_password']:
+            errors['confirm_password'] = "Password and Confirm password must match."
+
+        return errors
+
+
+    def login_validator(self, post_data):
+        errors = {}
+
+        user_list = User.objects.filter(email = post_data['email'])
+        if len(user_list) > 0:
+            user = user_list[0]
+            if not bcrypt.checkpw(post_data['password'].encode(), user.password.encode()):
+                errors['password'] = "Invalid Credentials"
+        else:
+            errors['email'] = "Invalid Credentials"
+
+        return errors
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
+    birth_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    objects = UserManager()
+
+class Party(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length = 255)
+    #  guest_list # future dev
+    date = models.DateField()
+    time = models.TimeField()
+    theme = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
---
+++ lectures_and_demos/w3/d3/fullCRUDproject/fullCRUDapp/views.py
@@ -0,0 +1,115 @@
+import bcrypt
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from .models import User, Party
+
+# Create your views here.
+def index(request):
+    if "uuid" in request.session:
+        return redirect("/dashboard")
+
+    return render(request, "index.html")
+
+
+def register(request):
+    errors = User.objects.registration_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect("/")
+    else:
+        hash_slinging_slasher = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        new_user = User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            birth_date = request.POST['birth_date'],
+            password = hash_slinging_slasher
+        )
+
+        request.session['uuid'] = new_user.id
+
+        return redirect("/dashboard")
+
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect("/")
+    else:
+        user = User.objects.get(email = request.POST['email'])
+
+        request.session['uuid'] = user.id
+
+        return redirect("/dashboard")
+
+
+def logout(request):
+    del request.session['uuid']
+
+    return redirect("/")
+
+
+def dashboard(request):
+    if "uuid" not in request.session:
+        return redirect("/")
+    context = {
+        "logged_in_user": User.objects.get(id = request.session['uuid']),
+        "all_users": User.objects.all(),
+        'all_parties': Party.objects.all()
+    }
+
+    return render(request, "dashboard.html", context)
+
+# ********************************************** parties app below
+def add_party_view(request):
+    return render(request, 'createParty.html')
+
+# CRUD C for create
+def add_party_process(request):
+    new_party = Party.objects.create(
+        name = request.POST['name'],
+        location = request.POST['loc'],
+        date = request.POST['date'],
+        time = request.POST['time'],
+        theme = request.POST['theme'],
+    )
+    return redirect('/dashboard')
+
+# CRUD U for update
+def update_party_view(request, party_id):
+    context = {
+        'party': Party.objects.get(id=party_id)
+    }
+    return render(request, 'editParty.html', context)
+
+def update_party_process(request, party_id):
+    party_to_update = Party.objects.get(id=party_id)
+    party_to_update.name = request.POST['name']
+    party_to_update.location = request.POST['loc']
+    party_to_update.date = request.POST['date']
+    # party_to_update.time = request.POST['time']
+    party_to_update.theme = request.POST['theme']
+
+    party_to_update.save()
+
+    return redirect('/dashboard')
+
+# CRUD D for delete
+def delete_party_view(request, party_id):
+    context = {
+        "party": Party.objects.get(id=party_id)
+    }
+    return render(request, 'deletePartyConfirm.html', context)
+
+def delete_party_process(request, party_id):
+    party_to_delete = Party.objects.get(id=party_id)
+    party_to_delete.delete()
+    return redirect('/dashboard')
