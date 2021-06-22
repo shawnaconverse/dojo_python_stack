c81cbbef94830065f108442723f09d9f57296b70
Shawn Allen Converse <shawn.a.converse@outlook.com>
2021-06-22 10:17:56 -0700

    w3d2 algos

--- algos/w3/d2/group2.js
+++ algos/w3/d2/group2.js
@@ -5,11 +5,13 @@
   taking advantage of the fact that the array is sorted .
 */
 
+
+
 const nums1 = [1, 3, 5, 6];
 const searchNum1 = 4;
 const expected1 = false;
 
-const nums2 = [4, 5, 6, 8, 12];
+const nums2 = [4, 5, 6, 8, 12]; 
 const searchNum2 = 5;
 const expected2 = true;
 
@@ -17,4 +19,42 @@ const nums3 = [3, 4, 6, 8, 12];
 const searchNum3 = 3;
 const expected3 = true;
 
-function binarySearch(sortedNums, searchNum) {}
+
+function binarySearch(sortedNums, searchNum) {
+  var mid = Math.floor(sortedNums.length/2);
+  var idx2 = sortedNums.length;
+  var idx1 = 0;
+  while(mid != idx1 || mid != idx2){
+      if(searchNum==sortedNums[mid]){
+        return true;
+      }
+      if(searchNum<sortedNums[mid]){
+          idx2 = mid;
+          mid = Math.floor((idx2+idx1)/2)
+      }
+      else if(searchNum>sortedNums[mid]){
+        idx1 = mid+1;
+        mid = Math.floor((idx2+idx1)/2)
+      }
+
+  }
+  return false;
+}
+
+
+function binarySearch(sortedNums, searchNum) {
+  let mid=Math.floor((sortedNums.length)/2);
+  let start=0;
+  let end=sortedNums.length;
+  while(mid!=start || mid!=end){
+      if(searchNum==sortedNums[mid]){
+          return true
+      } else if(searchNum<sortedNums[mid]){
+          end=mid;
+      } else {
+          start=mid+1;
+      }
+      mid=Math.floor((start+end)/2);
+  }
+  return false
+}
--- algos/w3/d2/group5.js
+++ algos/w3/d2/group5.js
@@ -16,5 +16,45 @@ const expected2 = true;
 const nums3 = [3, 4, 6, 8, 12];
 const searchNum3 = 3;
 const expected3 = true;
+// lets divide the length by two
+// odd length loop, even length loop
+// odd length loop --> 
+function binarySearch(sortedNums, searchNum) {
+  var len = Math.floor(sortedNums.length/2);
+  for (var i=0; i<sortedNums.length;i++) {
+    if (sortedNums[len]===searchNum) {
+      return true;
+    } else {
+      if (sortedNums[len]<searchNum) {
+        len = len/2
+      } else {
+        len = sortedNums.length-len
+      }
+    }
+  }
+}
 
-function binarySearch(sortedNums, searchNum) {}
+
+
+
+
+
+
+
+function binarySearch(arr, expect){
+  var start = 0;
+  var end = arr.length-1
+  while (start <= end){
+    var middle = Math.floor((start + end) / 2)
+    if (expect == arr[middle]){
+      return true;
+    }
+    if (expect > arr[middle]){
+      start = middle + 1;
+    }
+    if (expect < arr[middle]){
+      end = middle - 1;
+    }
+  }
+  return false
+}
