acf592dd521ac53a2ede4dbffe653c5f5280806d
Shawn Allen Converse <shawn.a.converse@outlook.com>
2021-07-06 11:44:04 -0700

    w1d1 morning commit

---
+++ lectures/w1/d2/intro.js
@@ -0,0 +1,22 @@
+function doSomething() {
+
+}
+
+// Camel Case
+var firstName = "Shawn"
+
+if (x > y) {
+  // do something
+} else if (y > x) {
+  // do something for the else if
+} else {
+  // do a default functionality
+}
+// JS and: && 
+// JS or: || 
+// JS not: !
+
+
+for (let i = 0; i < 10; i++) {
+  console.log("hello");
+}
