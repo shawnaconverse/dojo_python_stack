2af7f6204ea54d32bc64051b8667da7f08a17b94
Shawn Allen Converse <shawn.a.converse@outlook.com>
2021-07-15 16:10:03 -0700

    w2d4 afternoon MVC UD

--- lectures/w2/d4/full_stack/server.py
+++ lectures/w2/d4/full_stack/server.py
@@ -1,31 +1,5 @@
-from flask import Flask, render_template, redirect, request
-from dog import Dog
-
-app = Flask(__name__)
-app.secret_key = "keep it secret, keep it safe"
-
-
-@app.route("/")
-def index():
-    return render_template("index.html", all_dogs = Dog.get_all())
-
-
-@app.route("/dogs/new")
-def new_dog():
-    return render_template("new_dog.html")
-
-
-@app.route("/dogs/create", methods = ['POST'])
-def create_dog():
-    # print(request.form)
-    Dog.create(request.form)
-
-    return redirect("/")
-
-
-@app.route('/dogs/<int:dog_id>')
-def display_dog(dog_id):
-    return render_template("dog.html", dog = Dog.get_one({"id": dog_id}))
+from flask_app import app
+from flask_app.controllers import dog_controller
 
 
 if __name__ == "__main__":
